be5a3e1cbb28f72edd867f76a56bd0d71ca82869
Adhan <adhan8791@gmail.com>
2023-05-17 13:22:54 +0700

    otak atik

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -1,4 +1,4 @@
 function Section5() {
-  return<div className="text-blue-500 text-xl">Ini section 5</div>
+  return<div class="text-center text-green-500 bg-green-100 text-xl font-serif">Kenapa Memilih Determinix?</div>
 }
 export default Section5;

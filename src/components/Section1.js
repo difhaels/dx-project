8f731e6c9ea8e6fec0fb51712bda887b9f0d7e63
Wsnuhrtno <wisnuhartono999@gmail.com>
2023-05-17 11:47:00 +0700

    test

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,6 +1,7 @@
 function Section1() {
-  return <div className="text-green-500 text-center ">Jasa Foto Pernikahan dan Event</div>;
-
+  return <div className="text-green-500 text-center ">Jasa Foto Pernikahan dan Event 
+  <h1>PPPPp</h1></div>;
+  
 }
 
 export default Section1;
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,4 +1,5 @@
 function Section2() {
-  return <div class="text-center text-green-500 bg-green-100">Galeri</div>;
+  return <div class="text-center text-green-500 bg-green-100">Galeri</div>
+  <div>lorem ipsum </div>;
 }
 export default Section2;

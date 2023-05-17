779a6b40168fcd220c150124775566bb21e5a82d
Grouu <aruchevy31@gmail.com>
2023-05-17 11:24:48 +0700

    section 2 and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
 function Footer() {
-  return <div class="bg-slate-600 text-center">copyright 2023 create by Dx</div>;
+  return <div class="bg-slate-600 text-center">copyright 2023 create with❤️ by Dx</div>;
 }
 export default Footer;
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,4 +1,4 @@
 function Section2() {
-  return <div class="text-center text-green-500 bg-green-100">ini adalah Section2</div>;
+  return <div class="text-center text-green-500 bg-green-100">Galeri</div>;
 }
 export default Section2;

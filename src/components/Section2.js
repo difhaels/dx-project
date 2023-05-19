eddb0fee5ee4fd577e8b87c799267b649fa3e96a
albi <aruchevy@gmail.com>
2023-05-19 19:49:51 +0700

    segini aja dlu gung gw mau uji coba di web gw

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,4 +1,14 @@
 function Section2() {
-  return <div class="text-center text-green-500 bg-green-100">Galeri</div>;
+  return <div class="grid grid-cols-3 bg-[#F4B183] text-center md:gap-60">
+    <div>Tugas 1</div>
+    <div>Tugas 2</div>
+    <div>Tugas 3</div>
+    <div>Tugas 4</div>
+    <div>Tugas 5</div>
+    <div>Tugas 6</div>
+    <div>Tugas 7</div>
+    <div>Tugas 8</div>
+    <div>Tugas 9</div>
+</div>;
 }
 export default Section2;

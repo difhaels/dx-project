1116f379072d866c0670022027ef2659fd3c8ec9
SvX555 <svxgamer.555@gmail.com>
2023-05-22 11:56:57 +0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,31 @@
+import DXlogo2 from "./img/logo/DX-logo-2.png";
 function Navbar() {
-  return <div class="text-center mb-3 h-10 bg-slate-400">ini adalah navbar<a href="loginform.html">login</a></div>;
+  
+  return(
+    <nav class="bg-[rgb(112,54,0)]">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-10 w-10" src={DXlogo2} alt="Logo"></img>
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Beranda</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Tentang</a>
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Layanan</a>
+                <a href="./Login/loginform.html" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
+              </div>
+            </div>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-center md:ml-6">
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;

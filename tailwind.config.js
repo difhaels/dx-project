71971df9685e9468280699e620d11543bf5a8519
Agung Saputra <agungsaputradifh@gmail.com>
2023-05-24 12:43:20 +0700

    menambahkan responsive setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1530px",
+      // => @media (min-width: 1570px) { ... }
+      "3xl": "1630px",
+      // => @media (min-width: 1570px) { ... }
+    },
     extend: {},
   },
   plugins: [],

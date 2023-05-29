0292ecf2b7501e57eb6d8577f49f2775a4d3b4c9
Agung Saputra <agungsaputradifh@gmail.com>
2023-05-29 13:39:03 +0700

    section 2 sementara

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,10 +1,17 @@
+
+
 function Section2() {
   return (
-    <div class="px text-center text-green-500 bg-green-100">
-      <h1>Mau Foto Apa?</h1>
-      <a href="https://www.flaticon.com/free-icons/wedding" title="wedding icons">
-        Wedding icons created by Freepik - Flaticon
-      </a>
+    <div class=" text-center text-green-500 bg-green-100">
+      <h1 className="text-xl py-10">Mau Foto Apa?</h1>
+      <div className="grid grid-cols-3">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
     </div>
   );
 }

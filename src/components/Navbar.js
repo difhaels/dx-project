5832a8a1f9378f712f290f033a948066e2066262
Adhan <adhan8791@gmail.com>
2023-05-29 12:37:38 +0700

    bang

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 function Navbar() {
-  return <div class="text-center mb-3 h-10 bg-slate-400">INI ADALAH NAVBAR</div>;
+  return <div class="text-center mb-3 h-10 bg-slate-400 text">INI ADALAH NAVBAR</div>;
 }
 
 export default Navbar;

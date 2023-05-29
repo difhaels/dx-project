14a0ddc63be6e750c6f13b70622749cde207e993
Grouu <aruchevy31@gmail.com>
2023-05-29 10:46:51 +0700

    change hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,16 +10,16 @@ function Navbar() {
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <a href="#" class="text-black  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">
                   Beranda
                 </a>
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <a href="#" class="text-black  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">
                   Tentang
                 </a>
-                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <a href="#" class="text-black  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">
                   Layanan
                 </a>
-                <a href="./Login/loginform.html" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                <a href="./Login/loginform.html" class="text-black  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">
                   Login
                 </a>
               </div>

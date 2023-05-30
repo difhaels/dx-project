6103a3a405074dd3f3995d066400db9a80f7a7f4
Grouu <aruchevy31@gmail.com>
2023-05-30 16:13:06 +0700

    add our team

---
+++ src/Menu/Ourteam.js
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+const OurTeam = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>
+        <h1>Our Team</h1>
+        <button onClick={() => navigate("/")} className="bg-green-300 px-4 py-2 mt-5 rounded-md">
+          Back to Home
+        </button>
+      </div>
+    </>
+  );
+};
+export default OurTeam;

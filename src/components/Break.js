03e5249db94c5d82a83e71d7168b50784078f2af
Agung Saputra <agungsaputradifh@gmail.com>
2023-05-31 16:58:35 +0700

    added break section

---
+++ src/components/Break.js
@@ -0,0 +1,13 @@
+import "./css/break.css"
+
+const Break = () => {
+    return (<>
+        <div class="custom-shape-divider-top-1685526969">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="shape-fill"></path>
+            </svg>
+        </div>
+    </>)
+}
+
+export  default Break

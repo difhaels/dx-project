33d2e984ce2cf27df2bfdf0ce1eaf9b14ab73f5e
Adhan <adhan8791@gmail.com>
2023-05-31 12:54:39 +0700

    Revisi S4

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -2,13 +2,13 @@ function Section4() {
   return (
     <>
       <h1 className="text-xl py-10 text-center text-text ">Kenapa Memilih DX Photograph?</h1>
-      <div className="text-center mb-5 py-2">1. Expert Team</div>
-      <div className="text-center mb-5 py-2">2. Fast Action</div>
-      <div className="text-center mb-5 py-2">3. Great Quality</div>
-      <div className="text-center mb-5 py-2">4. Great Services</div>
-      <div className="text-center mb-5 py-2">5. Professional Studio</div>
-      <div className="text-center mb-5 py-2">6. Very East</div>
-      <div className="text-center mb-5 py-2 pb-10">7. Guaranteed</div>
+      <div className="text-center text-slate-500 mb-5 py-2">1. Expert Team</div>
+      <div className="text-center text-slate-500 mb-5 py-2">2. Fast Action</div>
+      <div className="text-center text-slate-500 mb-5 py-2">3. Great Quality</div>
+      <div className="text-center text-slate-500 mb-5 py-2">4. Great Services</div>
+      <div className="text-center text-slate-500 mb-5 py-2">5. Professional Studio</div>
+      <div className="text-center text-slate-500 mb-5 py-2">6. Very East</div>
+      <div className="text-center text-slate-500 mb-5 py-2 pb-10">7. Guaranteed</div>
     </>
   );
 }

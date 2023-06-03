6fadf650527d578cdb02165767186301a59dad08
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-03 21:07:15 +0700

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,14 @@ import DXlogo3 from './img/logo/DX-logo-3.png';
 import { useNavigate } from "react-router-dom"
 function Navbar() {
   const navigate = useNavigate();
-  return (
-    <div className="px bg-bg">
-      <div className="flex items-center justify-between">
-        <img src={DXlogo3} alt="Logo" width="60"></img>
-        <div className="flex">
-            {/* <a href="#" className="text-text  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">
-              Beranda
-            </a>
-            <a href="#" className="text-text  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">
-              Tentang
-            </a>
-            <a href="#" className="text-text  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">
-              Layanan
-            </a> */}
-            <button onClick={() => navigate("login")} className="text-text  hover:text-white px-3 py-2 rounded-lg text-sm font-medium">Login</button>
-        </div>
+  return (<>
+      <div className="px py-3 bg-bg flex items-center justify-between">
+          <img src={DXlogo3} alt="Logo" width="60"></img>
+          <div className="flex">
+              <button onClick={() => navigate("login")} className="text-white px-5 py-2 bg-emerald-800 shadow-xl hover:bg-emerald-700 rounded-lg text-sm font-medium">Login</button>
+          </div>
       </div>
-    </div>
+    </>
   );
 }
 export default Navbar;

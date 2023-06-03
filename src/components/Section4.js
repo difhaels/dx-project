a920d161c3190107fb9c7d82fbf1b3be542f5b5f
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-03 23:16:10 +0700

    edit section4

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -2,14 +2,14 @@ function Section4() {
   return (
     <>
       <div className="bg-[#E3F4F4] py-14">
-        <h1 className="text-2xl text-center text-text ">Kenapa Memilih DX Photograph?</h1>
-        <h1 className="text-center text-xl text-slate-500 mb-5 py-2">1. Expert Team</h1>
-        <h1 className="text-center text-xl text-slate-500 mb-5 py-2">2. Fast Action</h1>
-        <h1 className="text-center text-xl text-slate-500 mb-5 py-2">3. Great Quality</h1>
-        <h1 className="text-center text-xl text-slate-500 mb-5 py-2">4. Great Services</h1>
-        <h1 className="text-center text-xl text-slate-500 mb-5 py-2">5. Professional Studio</h1>
-        <h1 className="text-center text-xl text-slate-500 mb-5 py-2">6. Very East</h1>
-        <h1 className="text-center text-xl text-slate-500 mb-5 py-2">7. Guaranteed</h1>
+        <h1 className="text-2xl text-center text-text font-bold pb-5">Kenapa Memilih DX Photograph?</h1>
+        <h1 className="text-center text-xl text-slate-500 mb-5 py-1">1. Expert Team</h1>
+        <h1 className="text-center text-xl text-slate-500 mb-5 py-1">2. Fast Action</h1>
+        <h1 className="text-center text-xl text-slate-500 mb-5 py-1">3. Great Quality</h1>
+        <h1 className="text-center text-xl text-slate-500 mb-5 py-1">4. Great Services</h1>
+        <h1 className="text-center text-xl text-slate-500 mb-5 py-1">5. Professional Studio</h1>
+        <h1 className="text-center text-xl text-slate-500 mb-5 py-1">6. Very East</h1>
+        <h1 className="text-center text-xl text-slate-500 mb-5 py-1">7. Guaranteed</h1>
       </div>
     </>
   );

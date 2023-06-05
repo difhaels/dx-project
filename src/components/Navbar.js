b73191ebd66a4f00d28383271aa3a4ebb1f88870
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-05 14:15:32 +0700

    remove px sementara

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 function Navbar() {
   const navigate = useNavigate();
   return (<>
-      <div className="px py-3 bg-bg flex items-center justify-between">
+      <div className="py-3 bg-bg flex items-center justify-between">
           <img src={DXlogo3} alt="Logo" width="60"></img>
           <div className="flex">
               <button onClick={() => navigate("login")} className="text-white px-5 py-2 bg-emerald-800 shadow-xl hover:bg-emerald-700 rounded-lg text-sm font-medium">Login</button>

6f70717ab00ed60f38ab4a8e92f878c45e16ee6f
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-06 09:03:55 +0700

    gabut dikit

--- src/Menu/Graduation.js
+++ src/Menu/Graduation.js
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
+
 const Graduation = () => {
+  const [coba, setCoba] = useState(0);
+  const [sepuluh, setSepuluh] = useState("belum sepuluh")
+  useEffect(()=>{
+    coba >=  10 ? setSepuluh("sudah sepuluh") : setSepuluh("belum sepuluh");
+  }, [coba])
+
   const navigate = useNavigate();
   return (
     <>
@@ -9,6 +18,9 @@ const Graduation = () => {
         <button onClick={() => navigate("/")} className="bg-green-300 px-4 py-2 mt-5 rounded-md">
           Back to Home
         </button>
+        <h1>{coba}</h1>
+        <button onClick={() => {setCoba(coba+1)}}>+</button>
+        <h1>{sepuluh}</h1>
       </div>
     </>
   );

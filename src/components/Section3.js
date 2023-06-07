f20b530ed7d7a94e3a3aa215b589c4255affe076
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-07 19:59:13 +0700

    import img  section 3

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,3 +1,10 @@
+import img1 from "./img/section3/img1.jpg"
+import img2 from "./img/section3/img2.jpg"
+import img3 from "./img/section3/img3.jpg"
+import img4 from "./img/section3/img4.jpg"
+import img5 from "./img/section3/img5.jpg"
+import img6 from "./img/section3/img6.jpg"
+
 function Section3() {
   return (<>
     <div className="px flex justify-center py-60">

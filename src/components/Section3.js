42f80d7b604312a1b5f4f642760379c5f5bcdd22
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-07 20:15:37 +0700

    mini gallery v1

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -7,8 +7,16 @@ import img6 from "./img/section3/img6.jpg"
 
 function Section3() {
   return (<>
-    <div className="px flex justify-center py-60">
-      <h1>Mini Gallery here</h1>
+    <div className="px py-16">
+      <h1 className="text-center text-xl text-slate-900 font-semibold pb-10">Mini Gallery</h1>
+      <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
+        <img src={img1} className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={img2} className="h-[300px] w-[420px]  border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={img3} className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={img4} className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={img5} className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={img6} className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+      </div>
     </div>
   </>);
 }

dbe4f773b0f0a2b57f4693fa8c951ad177b455cf
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-10 17:31:11 +0700

    menambahkan foto dan prototype

--- src/Menu/Etc.js
+++ src/Menu/Etc.js
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom"
 
+import img3 from "./img/etc/img3.jpg"
+
 const Etc = () => {
     const navigate = useNavigate();
     return (<>
-        <div>
+        <div className="px">
             <h1>ini Etc bro</h1>
             <button onClick={() => navigate('/')} className="bg-green-300 px-4 py-2 mt-5 rounded-md">Back to Home</button>
         </div>

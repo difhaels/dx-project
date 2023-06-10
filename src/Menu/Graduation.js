f2024ce2758e6cd39f7c62e051ccaf72f17b3ec5
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-10 17:35:09 +0700

    menu v1

--- src/Menu/Graduation.js
+++ src/Menu/Graduation.js
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
+import img3 from "./img/graduation/img3.jpg"
 
 const Graduation = () => {
-  const [coba, setCoba] = useState(0);
-  const [sepuluh, setSepuluh] = useState("belum sepuluh")
-  useEffect(()=>{
-    coba >=  10 ? setSepuluh("sudah sepuluh") : setSepuluh("belum sepuluh");
-  }, [coba])
 
   const navigate = useNavigate();
   return (
     <>
-      <div>
-        <h1>ini Graduation bro</h1>
-        <button onClick={() => navigate("/")} className="bg-green-300 px-4 py-2 mt-5 rounded-md">
-          Back to Home
-        </button>
-        <h1>{coba}</h1>
-        <button onClick={() => {setCoba(coba+1)}}>+</button>
-        <h1>{sepuluh}</h1>
+      <div className="px py-10 bg-slate-100">
+        <h1 className="font-bold text-2xl text-center pb-10">Graduation</h1>
+        <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
+          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        </div>
+        <div>
+          <h1 className="font-bold text-2xl text-center py-10">Paket penjualan</h1>
+        </div>
+        <div className="flex justify-center">
+          <button onClick={() => navigate('/')} className="bg-green-300 px-4 py-2 mt-5 rounded-md">Back to Home</button>
+        </div>
       </div>
     </>
   );

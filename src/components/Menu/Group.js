dfdc7f22ca7b62f83d9d4997452d8367ff0e295d
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-16 20:34:55 +0700

    add foto group by albi

--- src/components/Menu/Group.js
+++ src/components/Menu/Group.js
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-import img3 from "./img/img1.jpg"
+import group1 from "./img/group1.jpg"
+import group2 from "./img/group2.jpg"
+import group3 from "./img/group3.jpg"
+import group4 from "./img/group4.jpg"
 
 const Group = () => {
   const navigate = useNavigate();
@@ -9,12 +12,10 @@ const Group = () => {
       <div className="px py-10 bg-slate-100">
         <h1 className="font-bold text-2xl text-center pb-10">Group</h1>
         <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={group1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={group2} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={group3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={group4} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
         </div>
         <div>
           <h1 className="font-bold text-2xl text-center py-10">Paket penjualan</h1>

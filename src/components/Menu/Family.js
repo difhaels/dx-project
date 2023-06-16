983874a2987ee72239370b899ed89f4fbe5ab6e6
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-16 20:31:58 +0700

    add foto family by albi

--- src/components/Menu/Family.js
+++ src/components/Menu/Family.js
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-import img3 from "./img/img1.jpg"
+import family1 from "./img/family1.jpg"
+import family2 from "./img/family2.jpg"
+import family3 from "./img/family3.jpg"
+import family4 from "./img/family4.jpg"
 
 const Family = () => {
   const navigate = useNavigate();
@@ -9,12 +12,10 @@ const Family = () => {
       <div className="px py-10 bg-slate-100">
         <h1 className="font-bold text-2xl text-center pb-10">Family</h1>
         <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={family1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={family2} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={family3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={family4} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
         </div>
         <div>
           <h1 className="font-bold text-2xl text-center py-10">Paket penjualan</h1>

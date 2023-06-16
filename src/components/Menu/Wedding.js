e034d91449e35b65cedb4912cdeaa7797a3051a2
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-16 20:36:34 +0700

    add foto wedding by albi

--- src/components/Menu/Wedding.js
+++ src/components/Menu/Wedding.js
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-import img3 from "./img/img1.jpg"
+import wedding1 from "./img/wedding1.jpg"
+import wedding2 from "./img/wedding2.jpg"
+import wedding3 from "./img/wedding3.jpg"
+import wedding4 from "./img/wedding4.jpg"
 
 const Wedding = () => {
   const navigate = useNavigate();
@@ -9,12 +12,10 @@ const Wedding = () => {
       <div className="px py-10 bg-slate-100">
         <h1 className="font-bold text-2xl text-center pb-10">Wedding</h1>
         <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={wedding1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={wedding2} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={wedding3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={wedding4} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
         </div>
         <div>
           <h1 className="font-bold text-2xl text-center py-10">Paket penjualan</h1>

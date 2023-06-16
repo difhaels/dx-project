d873d29b987f0adc75d40963ca8c450122383208
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-16 20:50:31 +0700

    change section3

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,21 +1,17 @@
-import img1 from "./img/section3/img1.jpg"
-import img2 from "./img/section3/img2.jpg"
-import img3 from "./img/section3/img3.jpg"
-import img4 from "./img/section3/img4.jpg"
-import img5 from "./img/section3/img5.jpg"
-import img6 from "./img/section3/img6.jpg"
+import family1 from "./img/section3/family1.jpg"
+import graduation1 from "./img/section3/graduation1.jpg"
+import group1 from "./img/section3/group1.jpg"
+import group2 from "./img/section3/group2.jpg"
 
 function Section3() {
   return (<>
     <div className="px py-16 bg-slate-100">
       <h1 className="text-center text-xl text-slate-900 font-semibold pb-10">Mini Gallery</h1>
       <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
-        <img src={img1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={img2} alt="img-gallery" className="h-[300px] w-[420px]  border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={img4} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={img5} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={img6} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={family1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={graduation1} alt="img-gallery" className="h-[300px] w-[420px]  border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={group1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={group2} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
       </div>
     </div>
   </>);

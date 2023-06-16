89c607fcacbda32db761d5735c37a1a30ce82abc
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-16 20:35:48 +0700

    add foto pet by albi

--- src/components/Menu/Pet.js
+++ src/components/Menu/Pet.js
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-import img3 from "./img/img1.jpg"
+import pet1 from "./img/pet1.jpg"
+import pet2 from "./img/pet2.jpg"
+import pet3 from "./img/pet3.jpg"
+import pet4 from "./img/pet4.jpg"
 
 const Pet = () => {
   const navigate = useNavigate();
@@ -9,12 +12,10 @@ const Pet = () => {
       <div className="px py-10 bg-slate-100">
         <h1 className="font-bold text-2xl text-center pb-10">Pet</h1>
         <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
-          <img src={img3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={pet1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={pet2} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={pet3} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
+          <img src={pet4} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
         </div>
         <div>
           <h1 className="font-bold text-2xl text-center py-10">Paket penjualan</h1>

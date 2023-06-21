e0823bb6ed9947937bf50ac9d607ec2d0526a945
Agung Saputra <agungsaputradifh@gmail.com>
2023-06-21 20:35:03 +0700

    ubah section pesan

--- src/components/Pesan.js
+++ src/components/Pesan.js
@@ -1,7 +1,7 @@
 const Pesan = () => {
     return (<>
         <a href="#pesan" className="w-16 h-16 rounded-full text-3xl flex justify-center items-center bg-green-600 text-white fixed bottom-5 right-5 sm:right-20 md:right-32 lg:right-52 cursor-pointer">
-            +
+            <h1 className="absolute top-[20%]">+</h1>
         </a>
     </>);
 }

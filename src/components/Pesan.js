ee47ee619fb7074eccc15592db957fbf5889602b
albi <aruchevy@gmail.com>
2023-06-26 13:40:53 +0700

    z-10

--- src/components/Pesan.js
+++ src/components/Pesan.js
@@ -3,7 +3,7 @@ const Pesan = () => {
     <>
       <a
         href="#pesan"
-        className="px-10 py-11 text-text font-bold  hover:text-white rounded-lg text-md text-center shadow-xl flex justify-center items-center bg-bg fixed bottom-5 right-5 sm:right-20 md:right-32 lg:right-52 cursor-pointer z-999"
+        className="px-10 py-11 text-text font-bold  hover:text-white rounded-lg text-md text-center shadow-xl flex justify-center items-center bg-bg fixed bottom-5 right-5 sm:right-20 md:right-32 lg:right-52 cursor-pointer z-50"
       >
         <h1 className="absolute top-[20%]">Book Now!!</h1>
       </a>

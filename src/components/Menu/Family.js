9c8e4baf4d38d1cbb1c9b0d7a6bec7ac60081a19
Grouu <aruchevy31@gmail.com>
2023-06-26 09:08:25 +0700

    added paket penjualan

--- src/components/Menu/Family.js
+++ src/components/Menu/Family.js
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-import family1 from "./img/family1.jpg"
-import family2 from "./img/family2.jpg"
-import family3 from "./img/family3.jpg"
-import family4 from "./img/family4.jpg"
+import family1 from "./img/family1.jpg";
+import family2 from "./img/family2.jpg";
+import family3 from "./img/family3.jpg";
+import family4 from "./img/family4.jpg";
 
 const Family = () => {
   const navigate = useNavigate();
@@ -18,10 +18,54 @@ const Family = () => {
           <img src={family4} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl" />
         </div>
         <div>
-          <h1 className="font-bold text-2xl text-center py-10">Paket penjualan</h1>
+          <h1 className="font-bold text-3xl text-center py-10">Paket penjualan</h1>
         </div>
+
+        <div className="flex flex-wrap gap-6 justify-center  ">
+          <div className=" text-center shadow-lg">
+            <div className="bg-slate-400  rounded-t-xl py-6 px-32 text-white ">
+              <h1 className="font-bold text-2xl mb-2">Family</h1>
+              <p>Paket Reguler</p>
+            </div>
+            <div className="bg-[#F9F5F6]  rounded-b-lg">
+              <h1 className="pt-3">
+                Rp.<span className="text-4xl">550</span>.000
+              </h1>
+              <p className="mt-4">30 Menit Pemrotetan</p>
+              <div className="bg-slate-400 mt-3 py-[1px] w-[80%] mx-auto "></div>
+              <p className="mt-4">30 Menit Pemrotetan</p>
+              <div className="bg-slate-400 mt-3 py-[1px] w-[80%] mx-auto "></div>
+              <p className="mt-4">30 Menit Pemrotetan</p>
+              <div className="bg-slate-400 mt-3 py-[1px] w-[80%] mx-auto "></div>
+              <p className="mt-4 pb-10">30 Menit Pemrotetan</p>
+            </div>
+            <button className="px-5 py-2 bg-bg rounded-md mb-5 text-text hover:text-white">Book Now!!</button>
+            <p className="pb-5 text-text">Make ur Different Moment</p>
+          </div>
+          <div className=" text-center shadow-lg">
+            <div className="bg-slate-400  rounded-t-xl py-6 px-32 text-white ">
+              <h1 className="font-bold text-2xl mb-2">Family</h1>
+              <p>Paket Reguler</p>
+            </div>
+            <div className="bg-[#F9F5F6]  rounded-b-lg">
+              <h1 className="pt-3">
+                Rp.<span className="text-4xl">550</span>.000
+              </h1>
+              <p className="mt-4">30 Menit Pemrotetan</p>
+              <div className="bg-slate-400 mt-3 py-[1px] w-[80%] mx-auto "></div>
+              <p className="mt-4">30 Menit Pemrotetan</p>
+              <div className="bg-slate-400 mt-3 py-[1px] w-[80%] mx-auto "></div>
+              <p className="mt-4">30 Menit Pemrotetan</p>
+              <div className="bg-slate-400 mt-3 py-[1px] w-[80%] mx-auto "></div>
+              <p className="mt-4 pb-10">30 Menit Pemrotetan</p>
+            </div>
+            <button className="px-5 py-2 bg-bg rounded-md mb-5 text-text hover:text-white">Book Now!!</button>
+            <p className="pb-5 text-text">Make ur Different Moment</p>
+          </div>
+        </div>
+
         <div className="flex justify-center">
-          <button onClick={() => navigate("/")} className="bg-bg px-4 py-2 mt-5 rounded-md hover:text-white">
+          <button onClick={() => navigate("/")} className="bg-bg px-4 py-2 mt-10 rounded-md text-text hover:text-white">
             Back to Home
           </button>
         </div>

6a97619e3631ec593076b036f096624e0d7a9bca
Grouu <aruchevy31@gmail.com>
2023-06-26 11:14:58 +0700

    change pesan

--- src/components/Pesan.js
+++ src/components/Pesan.js
@@ -1,9 +1,11 @@
 const Pesan = () => {
-    return (<>
-        <a href="#pesan" className="w-16 h-16 rounded-full text-3xl flex justify-center items-center bg-green-600 text-white fixed bottom-5 right-5 sm:right-20 md:right-32 lg:right-52 cursor-pointer">
-            <h1 className="absolute top-[20%]">+</h1>
-        </a>
-    </>);
-}
+  return (
+    <>
+      <a href="#pesan" className="px-10 py-11 text-text font-bold hover:text-white rounded-lg text-md text-center shadow-xl flex justify-center items-center bg-bg fixed bottom-5 right-5 sm:right-20 md:right-32 lg:right-52 cursor-pointer">
+        <h1 className="absolute top-[20%]">Book Now!!</h1>
+      </a>
+    </>
+  );
+};
 
-export default Pesan
+export default Pesan;

feb7f8af3a57f3ea1d97751f7ce9dedfae47a37a
Wsnuhrtno <wisnuhartono999@gmail.com>
2023-06-27 15:28:43 +0700

    Change section ourteam (sementara)

--- src/components/Menu/Ourteam.js
+++ src/components/Menu/Ourteam.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import aru from "./img/profil/foto-aru.jpeg";
+import wisnu from "./img/profil/inu.jpg";
 
 const OurTeam = () => {
   const navigate = useNavigate();
@@ -50,10 +51,10 @@ const OurTeam = () => {
           </div>
 
           <div className="mt-10 bg-bg px-3 py-5 rounded-lg shadow-xl text-text ">
-            <img src={aru} className="w-[150px] rounded-lg" />
-            <p className="text-center pt-1 font-bold">Aru Chevy Rasyid</p>
+            <img src={wisnu} className="w-[150px] rounded-lg" />
+            <p className="text-center pt-1 font-bold">Wisnu Hartono</p>
             <div className="flex justify-center">
-              <a className="bg-text px-3 py-2 mt-2 rounded-md text-bg shadow-2xl hover:text-white" href="https://www.instagram.com/aruchvy_/" target="_blank">
+              <a className="bg-text px-3 py-2 mt-2 rounded-md text-bg shadow-2xl hover:text-white" href="https://www.instagram.com/wsnuhrtno_._/" target="_blank">
                 Social Media
               </a>
             </div>
@@ -100,7 +101,7 @@ const OurTeam = () => {
           </div>
         </div>
 
-        <div className="flex justify-center text-text">
+        <div className="flex justify-center text-text m-20">
           <button onClick={() => navigate("/")} className="bg-bg px-4 py-2 mt-9 rounded-md hover:text-white ">
             Back to Home
           </button>

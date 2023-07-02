32e6395cf2198e6fbd8762e327bf559cf95b7701
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-02 08:25:11 +0700

    ubah section 3

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -8,10 +8,10 @@ function Section3() {
     <div className="px py-16 bg-slate-100">
       <h1 className="text-center text-xl text-slate-900 font-semibold pb-10">Mini Gallery</h1>
       <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
-        <img src={family1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={graduation1} alt="img-gallery" className="h-[300px] w-[420px]  border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={group1} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={group2} alt="img-gallery" className="h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={family1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={graduation1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px]  border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={group1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={group2} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
       </div>
     </div>
   </>);

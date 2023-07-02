6f8a36f342c3b459a63b6ec50dd41aa72a698748
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-02 20:08:22 +0700

    ubah register dan database

--- src/components/Menu/Logister/Register.js
+++ src/components/Menu/Logister/Register.js
@@ -24,7 +24,7 @@ function Register() {
     } else if (password !== password2) { 
       alert ("Kesalahan dalam konfirmasi password");
     } else {
-      const url = "http://localhost/enquiry.php";
+      const url = "http://localhost/determinix/register.php";
 
       let fdata = new FormData();
       fdata.append('username',username);

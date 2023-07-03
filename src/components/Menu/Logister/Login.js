970fd23680f591cd1692c3f933a5912f0f797c50
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-03 11:39:17 +0700

    added login v1

--- src/components/Menu/Logister/Login.js
+++ src/components/Menu/Logister/Login.js
@@ -1,118 +1,94 @@
-import { useEffect, useState } from "react";
 import close from "./cancel.png"
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
   const navigate = useNavigate();
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [msg, setMsg] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  useEffect(() => {
-    let login = localStorage.getItem("login");
-    if(login){
-        navigate("/");
-    }
-    let loginStatus = localStorage.getItem("loginStatus");
-    if(loginStatus){
-        setError(loginStatus);
-        setTimeout(function(){
-            localStorage.clear();
-            window.location.reload();
-        }, 3000);
-    }
-    setTimeout(function(){
-        setMsg("");
-    }, 5000);
-}, [msg]);
+  const handleSubmit = () => {
+    const url = "http://localhost/determinix/login.php";
 
-const handleInputChange = (e, type) => {
-  switch(type){
-      case "username":
-          setError("");
-          setUsername(e.target.value);
-          if(e.target.value === ""){
-              setError("Username tidak boleh kosong!");
-          }
-          break;
-      case "password":
-          setError("");
-          setPassword(e.target.value);
-          if(e.target.value === ""){
-              setError("Password tidak boleh kosong!");
-          }
-          break;
-      default:
-  }
-}
+    let fdata = new FormData();
+    fdata.append("username", username);
+    fdata.append("password", password);
 
-function loginSubmit(){
-  if(username !== "" && password != ""){
-      var url = "http://localhost/determinix/login.php";
-      var headers = {
-          "Accept": "application/json",
-          "Content-type": "application/json"
-      };
-      var Data = {
-          username: username,
-          nama: password
-      };
-      fetch(url, {
-          method: "POST",
-          headers: headers,
-          body: JSON.stringify(Data)
-      }).then((response) => response.json())
-      .then((response) => {
-          if(response[0].result === "Invalid username!" || response[0].result === "Invalid password!"){
-              setError(response[0].result);
-          }
-          else{
-              setMsg(response[0].result);
-              setTimeout(function(){
-                  localStorage.setItem("login", true);
-                  navigate("/");
-              }, 5000);
-          }
-      }).catch((err) => {
-          setError(err);
-          console.log(err);
+    axios
+      .post(url, fdata)
+      .then(response => {
+        const data = response.data;
+        if (data === "Success!") {
+          // Login berhasil, navigasikan pengguna ke halaman beranda
+          navigate("/");
+        } else {
+          // Login gagal, tampilkan pesan kesalahan
+          setErrorMessage("Username atau password salah");
+        }
       })
-  }
-  else{
-      setError("All field are required!")
-  }
-}
+      .catch(error => {
+        // Kesalahan dalam permintaan
+        setErrorMessage("An error occurred. Please try again later.");
+      });
+  };
 
   return (
     <>
-      <div className="bg-[#ABC4AA] min-h-screen px-5 flex items-center justify-center">
-        <form action="" method="post">
+      <div className="bg-[#ABC4AA] min-h-screen flex items-center justify-center px-5">
+        <form>
           <div className="w-full max-w-lg bg-white rounded-lg shadow-lg">
             <div className="flex justify-between items-center px-4 py-2">
               <h1 className="text-2xl font-bold">Login</h1>
-              <button onClick={() => navigate('/')} className="px-2 py-1 w-10"><img alt="close" className="hover:opacity-75" src={close}></img></button>
+              <button
+                onClick={() => navigate("/")}
+                className="px-2 py-1 w-10"
+              >
+                <img alt="close" className="hover:opacity-75" src={close}></img>
+              </button>
             </div>
             <hr />
-            <p>
-              {
-                error !== ""?
-                <span>{error}</span>:
-                <span>{msg}</span>
-              }
-            </p>
             <div className="px-6 py-4">
-              <input type="text" placeholder="Username" name="username" value={username} onChange={(e) => handleInputChange(e, "username")} className="logister-input" />
-              <input type="password" placeholder="Password" name="password" value={password} onChange={(e) => handleInputChange(e, "password")} className="logister-input" />
-              <button type="submit" name="login" defaultValue="Login" onClick={loginSubmit} className="bg-blue-500 text-white hover:bg-blue-400 px-4 py-2 rounded-lg w-full mb-4">Login</button>
-              <p className="text-sm">Belum punya akun? <button onClick={() => navigate('../register')} className="text-blue-500 border-none">Daftar Disini</button></p>
+              <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="logister-input"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="logister-input"
+              />
+              <button
+                type="button"
+                name="send"
+                id="send"
+                value="SEND"
+                onClick={handleSubmit}
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg w-full mb-4"
+              >
+                Login
+              </button>
+              {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+              <p className="text-sm">
+                Belum punya akun?{" "}
+                <button onClick={() => navigate("/register")} className="text-blue-500">
+                  Daftar Disini
+                </button>
+              </p>
             </div>
           </div>
         </form>
       </div>
     </>
-    
   );
 }
 

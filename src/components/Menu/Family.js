29d1b50b03620f23245a446d59b65c576fd231aa
Agung Saputra <agungsaputradifh@gmail.com>
2023-07-04 07:34:24 +0700

    ubah pesan family

--- src/components/Menu/Family.js
+++ src/components/Menu/Family.js
@@ -9,6 +9,19 @@ const Family = () => {
   const navigate = useNavigate();
   const storedUsername = localStorage.getItem("username");
 
+  const paket = {
+    nama: "Family",
+    jenis: ["Reguler", "Eksklusif", "Member"],
+    harga: ["149.000", "249.000", "199.000"]
+  }
+
+  function text (nama, jenis, harga){
+    let pesan = `Checkout,%20${nama}%20Paket%20${jenis}%20Rp.${harga},-`;
+    return pesan;
+  }
+
+  const dumb = "http://wa.me/+62895337305533?text=" + text(paket.nama, paket.jenis[0], paket.harga[0]);
+  console.log(dumb)
   return (
     <>
       <div className="px py-10 bg-slate-100">
@@ -43,7 +56,7 @@ const Family = () => {
               <div className="line-paket"></div>
               <p className="mt-4 pb-10">All file copy ke HP/Google Drive </p>
             </div>
-            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+            <a href={"http://wa.me/+62895337305533?text=" + text(paket.nama, paket.jenis[0], paket.harga[0])} target="_blank" rel="noreferrer">
               <button className="button-paket" >Book Now!!</button>
             </a>
             <p className="pb-5 text-text">Make ur Different Moment</p>
@@ -67,7 +80,7 @@ const Family = () => {
               <div className="line-paket"></div>
               <p className="mt-4 pb-10">All file copy ke HP/Google Drive</p>
             </div>
-            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+            <a href={"http://wa.me/+62895337305533?text=" + text(paket.nama, paket.jenis[1], paket.harga[1])} target="_blank" rel="noreferrer">
               <button className="button-paket" >Book Now!!</button>
             </a>          
             <p className="pb-5 text-text">Make ur Different Moment</p>
@@ -93,7 +106,7 @@ const Family = () => {
               <div className="line-paket"></div>
               <p className="mt-4 pb-10">All file copy ke HP/Google Drive</p>
             </div>
-            <a href="https://fitgirl-repacks.to/" target="_blank" rel="noreferrer">
+            <a href={"http://wa.me/+62895337305533?text=" + text(paket.nama, paket.jenis[2], paket.harga[2])} target="_blank" rel="noreferrer">
               <button className="button-member">Book Now!!</button>
             </a>
             <p className="pb-5 text-text">Make ur Different Moment</p>

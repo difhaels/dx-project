f95760c9e4bbbdf8bad44878f9b50d2a46b9737d
SvX555 <svxgamer.555@gmail.com>
2023-07-06 14:36:57 +0700

    effect gempa section 2

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -5,13 +5,13 @@ import group2 from "./img/section3/group2.jpg"
 
 function Section3() {
   return (<>
-    <div className="px py-16 bg-slate-100">
+    <div className="px-4 py-16 bg-slate-100">
       <h1 className="text-center text-xl text-slate-900 font-semibold pb-10">Mini Gallery</h1>
       <div className="flex flex-wrap justify-center gap-7 sm:gap-14 pb-10">
-        <img src={family1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={graduation1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px]  border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={group1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
-        <img src={group2} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl"/>
+        <img src={family1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl transition duration-300 transform hover:scale-105" />
+        <img src={graduation1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl transition duration-300 transform hover:scale-105" />
+        <img src={group1} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl transition duration-300 transform hover:scale-105" />
+        <img src={group2} alt="img-gallery" className="h-[250px] sm:h-[300px] w-[420px] border-[15px] border-b-[40px] border-stone-50 shadow-2xl transition duration-300 transform hover:scale-105" />
       </div>
     </div>
   </>);
